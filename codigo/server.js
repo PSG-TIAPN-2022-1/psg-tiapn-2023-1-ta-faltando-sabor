9ce03124759cf178939c7fd5850712fc4564782d
Diego Aguiar <diego56bola@hotmail.com>
2023-06-11 01:37:40 -0300

    alteracao no server

--- codigo/server.js
+++ codigo/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 3000;
